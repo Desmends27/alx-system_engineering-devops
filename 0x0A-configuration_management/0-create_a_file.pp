7eee98ac94512f64154686de93db648c7df0cebc
desmends27 <desmondmends27@gmail.com>
2023-06-23 21:19:35 +0000

    test

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
-file { '/tmp/school':
-  path    => '/tmp/school',
+#create a file in /tmp
+
+file { 'school':
+  path  => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
 
