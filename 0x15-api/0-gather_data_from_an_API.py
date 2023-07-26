da32884cfecf1252fa763d54931df90f29ada90e
desmends27 <desmondmends27@gmail.com>
2023-07-26 02:38:16 +0000

    format errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         if entry['completed'] is True:
             completed += 1
         total += 1
-    print("Employee {} is done with tasks({}/{}:)".format(users['name'],
-                                                 completed, total))
+    print("Employee {} is done with tasks({}/{}):".format(users['name'],
+                                                          completed, total))
     for entry in todo_list:
-        print('\t{}'.format(entry['title']))
+        print(' \t{}'.format(entry['title']))
