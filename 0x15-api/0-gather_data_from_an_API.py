90aec7bb046c0c517aa43dece852e71fbb88e4d1
desmends27 <desmondmends27@gmail.com>
2023-07-26 02:33:15 +0000

    amount of tasks

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Gather data from an API """
+
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+    user_id = argv[1]
+    url1 = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    url2 = '{}/todos'.format(url1)
+    users = get(url1).json()
+    todo_list = get(url2).json()
+    completed = 0
+    total = 0
+    for entry in todo_list:
+        if entry['completed'] is True:
+            completed += 1
+        total += 1
+    print("{} is done with tasks({}/{}:)".format(users['name'],
+                                                 completed, total))
+    for entry in todo_list:
+        print('\t{}'.format(entry['title']))
---
+++ 0x15-api/README.md
@@ -0,0 +1,3 @@
+Basic usage of apis
+How to read data from them in CSV and JSON
+API used in this dir is https://jsonplaceholder.typicode.com
