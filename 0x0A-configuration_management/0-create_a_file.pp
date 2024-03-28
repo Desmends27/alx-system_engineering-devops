98021bfe06158a5b6b522d39b1f6d16112aefd57
Desmends27 <desmondopokumends2004@gmail.com>
2024-03-28 14:53:33 +0000

    test2

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-file { '/tmp/school':
-ensure  => 'file',
-mode    => '0744',
-path    => '/tmp/school',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet',
+file { 'school':
+  ensure  => 'present',
+  path      => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
+
