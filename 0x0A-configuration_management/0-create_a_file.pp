e43e220e54aa6ef560e66d17ecaf943ad867ceff
Desmends27 <desmondopokumends2004@gmail.com>
2024-03-28 14:40:06 +0000

    test

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-file { '/tmp':
-path       => '/tmp/school',
-mode      => '0744',
-owner      => 'www-data',
-group      => 'www-data',
-content      => 'I love Puppet',
+file { '/tmp/school':
+ensure  => 'file',
+mode    => '0744',
+path    => '/tmp/school',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 }
