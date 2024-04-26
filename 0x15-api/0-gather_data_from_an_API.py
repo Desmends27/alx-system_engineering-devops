5019369ea404927a6862f536ea67072bc7b6b557
Desmends27 <desmondopokumends2004@gmail.com>
2024-04-26 03:00:47 +0000

    done

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Gather data from an API """
+
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+    user_id = argv[1]
+    url1 = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    url2 = '{}/todos'.format(url1)
+    users = get(url1).json()
+    todo_list = get(url2).json()
+    completed = 0
+    total = 0
+    completed_list = []
+    for entry in todo_list:
+        if entry['completed'] is True:
+            completed += 1
+            completed_list.append(entry['title'])
+        total += 1
+    print("Employee {} is done with tasks({}/{}):".format(users['name'],
+                                                          completed, total))
+
+    for val in completed_list:
+        print("\t {}".format(val))
---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Gather data from an API  and use it to generate a list of employees"""
+
+from requests import get
+from sys import argv
+import json
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    url1 = f'{url}users'
+    users = get(url1).json()
+    final_dict = {}
+    for user in users:
+        url2 = '{}/{}/todos'.format(url1, user['id'],)
+        todos = get(url2).json()
+        todos_list = []
+        for action in todos:
+            task_dict = {
+                "username": user["username"],
+                "task": action["title"],
+                "completed": action["completed"],
+                }
+            todos_list.append(task_dict)
+        final_dict[user['id']] = todos_list
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(final_dict, f)
